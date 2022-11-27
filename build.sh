c74021b0ea765971772502ab8d67857060edbc6b
bertieaux jordan <bertieaux.jo@gmail.com>
2022-11-27 13:25:39 +0000

    update builder

--- build.sh
+++ build.sh
@@ -12,13 +12,13 @@ cp scripts/configurations .config
 make V=s -j5
 
 # update image builder
-#cd bin/targets/ar71xx/generic/
-#tar xvf openwrt-imagebuilder-ar71xx-generic.Linux-x86_64.tar.xz
-#mv openwrt-imagebuilder-ar71xx-generic.Linux-x86_64 ~/AR300M16-ext/openwrt-imagebuilder
-#rm -rfv openwrt-imagebuilder-ar71xx-generic.Linux-x86_64/
+cd /home/vagrant/WRT3200ACM/bin/targets/mvebu/cortexa9
+tar xvf openwrt-imagebuilder-mvebu-cortexa9.Linux-x86_64.tar.xz
+mv openwrt-imagebuilder-mvebu-cortexa9.Linux-x86_64/ /home/vagrant/WRT3200ACM/openwrt-imagebuilder
+rm -rfv openwrt-imagebuilder-mvebu-cortexa9.Linux-x86_64/
 
 # copy builder
-#cd  ~/AR300M16-ext/openwrt-imagebuilder/
-#cp -avr ../overlay .
-#mv overlay/build.sh build.sh
-#mv overlay/repositories.conf repositories.conf
+cd  ~/AR300M16-ext/openwrt-imagebuilder/
+cp -avr ../overlay .
+mv overlay/build.sh build.sh
+mv overlay/repositories.conf repositories.conf
